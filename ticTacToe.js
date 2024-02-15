676ae54fe2ebaec99b487d3c2db0a1109994d7a9
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-15 21:00:57 +0600

    re desiging this project

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,45 +1,8 @@
-const cells=[];
-const checkResult = [1,2,3,4,5,6,7,8,9];
-
-
-function cellCheck(cellID){
- 
-    let checkSymbol;
-    let symbolSizeOne;
-    let symbolSizeTwo;
-    let result;
-
-    if(cells.length <= 9 && !cells.includes(cellID) && !resultCheck()){
-
-        if(cells.length%2 === 0){
-            checkSymbol = `<i class="fa-solid fa-circle"></i>`;
-            symbolSizeOne = `md:text-[15svh]`;
-            symbolSizeTwo = `text-[21vw]`;
-            checkResult[Number(cellID)-1] = 'O';
-        }else{
-            checkSymbol = `<i class="fa-solid fa-xmark"></i>`;
-            symbolSizeOne = `md:text-[20svh]`;
-            symbolSizeTwo = `text-[29vw]`;
-            checkResult[Number(cellID)-1] = 'X';
-        }
-    
-        document.getElementById(cellID).innerHTML = checkSymbol;
-        document.getElementById(cellID).classList.add(symbolSizeOne, symbolSizeTwo);
-        cells.push(cellID);
-        console.log(cells);
-        result = resultCheck();
-        console.log(result);
-
-    }
-    
-}
-
-function resultCheck() {
-    if ((checkResult[0] === `X` && checkResult[1] === `X` && checkResult[2] === `X`) || (checkResult[3] === `X` && checkResult[4] === `X` && checkResult[5] === `X`) || (checkResult[6] === `X` && checkResult[7] === `X` && checkResult[8] === `X`) || (checkResult[0] === `X` && checkResult[4] === `X` && checkResult[8] === `X`) || (checkResult[2] === `X` && checkResult[4] === `X` && checkResult[6] === `X` || (checkResult[0] === `X` && checkResult[3] === `X` && checkResult[6] === `X`) || (checkResult[1] === `X` && checkResult[4] === `X` && checkResult[7] === `X`) || (checkResult[2] === `X` && checkResult[5] === `X` && checkResult[8] === `X`))) {
-        return `X`;
-    } else if (!checkResult.includes(1) && !checkResult.includes(2) && !checkResult.includes(3) && !checkResult.includes(4) && !checkResult.includes(5) && !checkResult.includes(6) && !checkResult.includes(7) && !checkResult.includes(8) && !checkResult.includes(9)) {
-        return `Draw`;
-    } else if ((checkResult[0] === `O` && checkResult[1] === `O` && checkResult[2] === `O`) || (checkResult[3] === `O` && checkResult[4] === `O` && checkResult[5] === `O`) || (checkResult[6] === `O` && checkResult[7] === `O` && checkResult[8] === `O`) || (checkResult[0] === `O` && checkResult[4] === `O` && checkResult[8] === `O`) || (checkResult[2] === `O` && checkResult[4] === `O` && checkResult[6] === `O`) || (checkResult[0] === `O` && checkResult[3] === `O` && checkResult[6] === `O`) || (checkResult[1] === `O` && checkResult[4] === `O` && checkResult[7] === `O`) || (checkResult[2] === `O` && checkResult[5] === `O` && checkResult[8] === `O`)) {
-        return `O`;
-    }
-}
+// Design the layout
+// adjust the size
+// make first player to choose symbol and lock it when the game starts (it will only happen before the first round. the the following round the symbol will be changed for the first player and will go on)
+// print the point of the cross and circle wins and ties and rounds number
+// make the event when the symbols will appear after clicking. we will use different functions for it.
+// every cell will have a value and by clicking on the div, it will modify and array. by which we will evaluate the the results
+// print the result. 
+// there will be a restart button at the bottom that will reset everything
