3a0362edc59c41ff09461ddc687ab393fbd472b7
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-27 01:38:32 +0600

    game is complete. now only a bit of media query left

--- line.js
+++ line.js
@@ -5,5 +5,23 @@ function line(){
     }else if((cellFour === cellFive && cellFive === cellSix) && (cellFour && cellFive && cellSix)){
 
         document.getElementById("row-2").style.width = "100%";
+    }else if((cellSeven === cellEight && cellEight === cellNine) && (cellSeven && cellEight && cellNine)){
+
+        document.getElementById("row-3").style.width = "100%";
+    }else if((cellOne === cellFour && cellFour === cellSeven) && (cellOne && cellFour && cellSeven)){
+
+        document.getElementById("colm-1").style.height = "100%";
+    }else if((cellTwo === cellFive && cellFive === cellEight) && (cellTwo && cellFive && cellEight)){
+
+        document.getElementById("colm-2").style.height = "100%";
+    }else if((cellThree === cellSix && cellSix === cellNine) && (cellThree && cellSix && cellNine)){
+
+        document.getElementById("colm-3").style.height = "100%";
+    }else if((cellOne === cellFive && cellFive === cellNine) && (cellOne && cellFive && cellNine)){
+
+        document.getElementById("diagonal-1").style.width = "100%";
+    }else if((cellThree === cellFive && cellFive === cellSeven) && (cellThree && cellFive && cellThree)){
+
+        document.getElementById("diagonal-2").style.width = "100%";
     }
 }
