c123b104bd9c756d2b48f9ec8b419079e0510e2d
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-27 00:19:34 +0600

    successfully checking the result

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,17 +2,18 @@ let firstPlayer;
 let secondPlayer;
 let playCount = document.getElementsByClassName('cells').length;
 
-const cellOne = document.getElementById("cell-1").innerText;
-const cellTwo = document.getElementById("cell-2").innerText;
-const cellThree = document.getElementById("cell-3").innerText;
-const cellFour = document.getElementById("cell-4").innerText;
-const cellFive = document.getElementById("cell-5").innerText;
-const cellSix = document.getElementById("cell-6").innerText;
-const cellSeven = document.getElementById("cell-7").innerText;
-const cellEight = document.getElementById("cell-8").innerText;
-const cellNine = document.getElementById("cell-9").innerText;
+let cellOne = document.getElementById("cell-1").innerText;
+let cellTwo = document.getElementById("cell-2").innerText;
+let cellThree = document.getElementById("cell-3").innerText;
+let cellFour = document.getElementById("cell-4").innerText;
+let cellFive = document.getElementById("cell-5").innerText;
+let cellSix = document.getElementById("cell-6").innerText;
+let cellSeven = document.getElementById("cell-7").innerText;
+let cellEight = document.getElementById("cell-8").innerText;
+let cellNine = document.getElementById("cell-9").innerText;
 
 
+let result;
 
 if (document.getElementById("player-select").innerText === 'circle') {
     document.getElementById("player-select").style.fontSize = "5vw";
@@ -55,7 +56,7 @@ const cells = document.getElementsByClassName('cells');
 
 for (let i of cells) {
     i.addEventListener('click', function () {
-        if (i.firstElementChild.innerText !== 'circle' && i.firstElementChild.innerText !== 'close' && playCount > 0) {
+        if (i.firstElementChild.innerText !== 'circle' && i.firstElementChild.innerText !== 'close' && playCount > 0 && !result) {
             playCount--;
 
             if (playCount % 2 === 0) {
@@ -77,6 +78,19 @@ for (let i of cells) {
                     document.getElementById("player-select").style.color = "#ed6677";
                 }
 
+                cellOne = document.getElementById("cell-1").innerText;
+                cellTwo = document.getElementById("cell-2").innerText;
+                cellThree = document.getElementById("cell-3").innerText;
+                cellFour = document.getElementById("cell-4").innerText;
+                cellFive = document.getElementById("cell-5").innerText;
+                cellSix = document.getElementById("cell-6").innerText;
+                cellSeven = document.getElementById("cell-7").innerText;
+                cellEight = document.getElementById("cell-8").innerText;
+                cellNine = document.getElementById("cell-9").innerText;
+
+                result = checkResult();
+                console.log(result);
+
             } else {
                 i.firstElementChild.innerText = secondPlayer;
                 document.getElementById("player-select").innerText = firstPlayer;
@@ -94,6 +108,19 @@ for (let i of cells) {
                     document.getElementById("player-select").style.fontSize = "5vw";
                     document.getElementById("player-select").style.color = "#ed6677";
                 }
+
+                cellOne = document.getElementById("cell-1").innerText;
+                cellTwo = document.getElementById("cell-2").innerText;
+                cellThree = document.getElementById("cell-3").innerText;
+                cellFour = document.getElementById("cell-4").innerText;
+                cellFive = document.getElementById("cell-5").innerText;
+                cellSix = document.getElementById("cell-6").innerText;
+                cellSeven = document.getElementById("cell-7").innerText;
+                cellEight = document.getElementById("cell-8").innerText;
+                cellNine = document.getElementById("cell-9").innerText;
+
+                result = checkResult();
+                console.log(result);
             }
 
         }
