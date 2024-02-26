a38977d766e0f5128e22e9cf411b08d2880f97a2
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-26 22:09:14 +0600

    player select done

---
+++ ticTacToe.js
@@ -0,0 +1,24 @@
+if(document.getElementById("player-select").innerText === 'circle'){
+    document.getElementById("player-select").style.fontSize = "5vw";
+    document.getElementById("player-select").style.color = "#ed6677";
+}
+
+let clickCount = 0;
+function playerSelect(){
+    clickCount++;
+
+    if(clickCount%2 !== 0){
+        document.getElementById("player-select").innerText = `close`;
+    }else{
+        document.getElementById("player-select").innerText = `circle`;
+    }
+
+    if(document.getElementById("player-select").innerText === 'circle'){
+        document.getElementById("player-select").style.fontSize = "5vw";
+        document.getElementById("player-select").style.color = "#ed6677";
+    }else{
+        document.getElementById("player-select").style.fontSize = "6vw";
+        document.getElementById("player-select").style.color = "#53bd9e";
+    }
+
+}
